f10e10f203320e41e09b482344b6b275e78f140c
KazunoriSumiya <Sumiya.kazunori@jp.panasonic.com>
2025-04-14 15:35:30 +0900

    test2 エラーとなる

--- model_compression_toolkit/ptq/keras/quantization_facade.py
+++ model_compression_toolkit/ptq/keras/quantization_facade.py
@@ -33,7 +33,8 @@ from model_compression_toolkit.core.runner import core_runner
 from model_compression_toolkit.ptq.runner import ptq_runner
 from model_compression_toolkit.metadata import create_model_metadata
 
-print("git test")
+print("git test
+      2")
 
 if FOUND_TF:
     from model_compression_toolkit.target_platform_capabilities.targetplatform2framework.attach2keras import \
